83b98cd4f5bb619ed85de3db596f826e50ebe864
Rihards Pacevics <rihardspacevics@gmail.com>
2017-05-18 11:02:27 +0200

    1st chyper query kind of done

--- server/dbFacade/neo4jSession.js
+++ server/dbFacade/neo4jSession.js
@@ -3,21 +3,26 @@
  */
 
 const driver = require('../connector/neo4j');
+const Book = require('../models/neo4jBook');
 
 const session = driver.getDriver();
 
 function getBookTitleByCityName(cityName) {
-    const resultPromise = session.run (
-        'MATCH (b: Book)-[:MENTIONS]->(c:City {name: $cityName}) RETURN b.title, b.author',
+    const resultPromise = session.run(
+        'MATCH (book: Book)-[:MENTIONS]->(c:City {name: $cityName}) RETURN book',
         {cityName: cityName}
     );
     resultPromise.then(result => {
+
+        const books = result.records.map(r => new Book(r.get('book')));
+        console.log(books);
+        return books;
         session.close();
-        const singleRecord = result.records[0];
-        const node = singleRecord.get(0);
-        console.log(node);
     })
-}
+        .catch((error) => {
+            console.error(error);
+        });
+};
 
 // just testing
 function insertBook() {
@@ -26,7 +31,7 @@ function insertBook() {
     const cities = ['Athens', 'London']
     const resultPromise = session.run(
         'CREATE (a: Book {title: $title, author: $author, cities: $cities}) RETURN a',
-        {title: title, author:author, cities: cities}
+        {title: title, author: author, cities: cities}
     );
     resultPromise.then(result => {
         session.close();
@@ -41,7 +46,7 @@ function insertBook() {
 function dropNeo4j() {
     session.run(
         'MATCH (n) DELETE n'
-    ).then( result => {
+    ).then(result => {
         session.close();
         console.log(result);
     })
