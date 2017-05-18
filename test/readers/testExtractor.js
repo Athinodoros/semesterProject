ea5dec59cbfcb90206b926973c97275803545a67
Athinodoros Sgouromallis <athinodoros.sgouromallis@hotmail.com>
2017-05-18 12:06:29 +0300

    readers init

---
+++ test/readers/testExtractor.js
@@ -0,0 +1,17 @@
+/**
+ * Created by Athinodoros on 5/16/2017.
+ */
+
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+var chaiAsPromised = require('chai-as-promised');
+var Promise = require('promise');
+var connector = require('../../server/connector/connector');
+chai.use(chaiAsPromised);
+
+describe('Tests for book model', function () {
+
+
+    
+})
